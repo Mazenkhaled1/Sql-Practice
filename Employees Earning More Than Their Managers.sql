148573b9285860914e6322c637933ef5862f3c09
mazen khaled <112590279+Mazenkhaled1@users.noreply.github.com>
2026-04-19 11:52:20 +0200

    Solve: Employees Earning More Than Their Managers

---
+++ Employees Earning More Than Their Managers.sql
@@ -0,0 +1,9 @@
+/*
+Write a solution to find the employees who earn more than their managers.
+Return the result table in any order.
+*/
+SELECT e2.name as Employee
+FROM employee e1
+INNER JOIN employee e2 ON e1.id = e2.managerID
+WHERE
+e1.salary < e2.salary
