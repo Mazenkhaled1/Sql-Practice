b63a48f18caf8d1563385203b33bfa061d9145cc
mazen khaled <112590279+Mazenkhaled1@users.noreply.github.com>
2026-04-21 13:09:48 +0200

    Solve: Duplicate Emails

---
+++ Duplicate Emails.sql
@@ -0,0 +1,9 @@
+/*
+Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+Return the result table in any order.
+*/
+
+SELECT email
+FROM person
+GROUP BY email 
+HAVING COUNT(email) > 1 
